51c84be98efb1e1334650d2ce1374cf8adbf4a45
usern <999998@inbox.ru>
2013-09-29 23:22:38 +0400

    lab2_0

---
+++ Consolelogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_0
+{
+    public class Consolelogger : Ilogger
+    {
+        private Int32 number = 0;
+        public void Log(Severity s, String msg)
+        {
+            switch (s)
+            {
+                case Severity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Severity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case Severity.Info:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
+            for (int i = 0; i < number; ++i)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(msg);
+
+        }
+
+        public void Indent()
+        {
+            number += 2;
+        }
+
+        public void Unindent()
+        {
+            if (number != 0)
+            {
+                number -= 2;
+            }
+        }
+    }
+}
---
+++ Ilogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_0
+{
+    interface Ilogger
+    {
+        void Log(Severity s, String msg);
+        void Indent();
+        void Unindent();
+    }
+
+    public enum Severity
+    {
+        Error, Warning, Info
+    }
+}
